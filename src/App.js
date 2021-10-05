3395d042c2a87aca0ef8f129e7c97d9c240e7967
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-10-05 13:43:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+width: 100%;
+height: 100%;
+background-color: #1c2127;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+color: #fff;
+`;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AppContainer>Object Detection!</AppContainer>;
 }
 
 export default App;
